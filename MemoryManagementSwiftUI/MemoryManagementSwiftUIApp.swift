8f1669f92e62b01d99b15717d39fc22cdf4ee486
Akshay Gandal <akshaydgandal@gmail.com>
2026-01-30 18:49:01 +0530

    Initial Commit

---
+++ MemoryManagementSwiftUI/MemoryManagementSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryManagementSwiftUIApp.swift
+//  MemoryManagementSwiftUI
+//
+//  Created by Akshay Gandal on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryManagementSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryManagementSwiftUITests/MemoryManagementSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryManagementSwiftUITests.swift
+//  MemoryManagementSwiftUITests
+//
+//  Created by Akshay Gandal on 30/01/26.
+//
+
+import Testing
+
+struct MemoryManagementSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
